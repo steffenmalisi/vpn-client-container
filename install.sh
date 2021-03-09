a9d60fccd5b55919c2a02573fc0394a832b107ab
steffenmalisi <steffenmalisi@users.noreply.github.com>
2021-03-09 18:35:52 +0100

    Add check for multipass installation

--- install.sh
+++ install.sh
@@ -9,6 +9,14 @@ CONTAINER_NAME=${1:-vpn}
 #multipass launch -vvvv --disk 3G --mem 512M --name $CONTAINER_NAME
 #multipass exec $CONTAINER_NAME /opt/init/init.sh
 
+if ! type "multipass2" >/dev/null 2>&1; then
+  echo "Multipass is not available on your system."
+  if [[ "$OSTYPE" =~ ^darwin ]]; then
+    echo "You can install it using 'brew install multipass'"
+  fi
+  exit 1;
+fi
+
 multipass launch -vvvv --disk 3G --mem 1G --name $CONTAINER_NAME --cloud-init container-config.yml
 multipass mount $(pwd)/scripts $CONTAINER_NAME:/opt
 multipass mount $(pwd)/config $CONTAINER_NAME:/home/ubuntu/.config
